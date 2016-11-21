1551cac156cfffcde2f0735a9390ffebe25f80ac
wesleyfriedman <friedman.wesley@gmail.com>
2016-11-21 14:53:05 -0500

    Done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,27 @@
-// define your actions here!
+export function addMovie(movie) {
+  return{
+    type: 'ADD_MOVIE',
+    movie: movie
+  }
+}
+
+export function deleteMovie(movie) {
+  return {
+    type: 'DELETE_MOVIE',
+    movie: movie
+  }
+}
+
+export function updateUsername(user) {
+  return {
+    type: 'UPDATE_USERNAME',
+    username: user
+  }
+}
+
+export function resetUsername(user) {
+  return{
+    type: 'RESET_USERNAME',
+    username: 'default'
+  }
+}
